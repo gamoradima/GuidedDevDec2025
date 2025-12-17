7fcc527a398d5260de0e6ab3e1b2fbf14d5d5c99
gamoradima <gamoradima@gmail.com>
2025-12-17 12:16:03 +0200

    Preconfigured page example, web service example

---
+++ UsrYacht/Autogenerated/Src/UsrYachtServiceSchema.UsrYacht.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: UsrYachtServiceSchema
+
+	/// <exclude/>
+	public class UsrYachtServiceSchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public UsrYachtServiceSchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public UsrYachtServiceSchema(UsrYachtServiceSchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("468a0019-052b-4f2f-9ecd-2b7ed19cd68e");
+			Name = "UsrYachtService";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("e90a136e-6038-481b-9e29-194c764113d0");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,171,204,47,45,82,112,86,86,72,206,79,73,85,200,72,45,74,213,3,1,0,126,139,23,212,22,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("468a0019-052b-4f2f-9ecd-2b7ed19cd68e"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
---
+++ UsrYacht/Schemas/UsrAverageTicketPriceOutputPage/UsrAverageTicketPriceOutputPage.js
@@ -0,0 +1,108 @@
+define("UsrAverageTicketPriceOutputPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "PageTitle",
+				"values": {
+					"caption": "#MacrosTemplateString(#ResourceString(PageTitle_caption)#)#",
+					"visible": true,
+					"headingLevel": "label"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Tabs",
+				"values": {
+					"styleType": "default",
+					"mode": "tab",
+					"bodyBackgroundColor": "primary-contrast-500",
+					"selectedTabTitleColor": "auto",
+					"tabTitleColor": "auto",
+					"underlineSelectedTabColor": "auto",
+					"headerBackgroundColor": "auto",
+					"allowToggleClose": true
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "AvgPrice",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PageParameters_UsrIntegerParameter1_es6vo2y",
+					"control": "$PageParameters_UsrIntegerParameter1_es6vo2y",
+					"readonly": true,
+					"placeholder": "",
+					"labelPosition": "auto",
+					"tooltip": "",
+					"visible": true
+				},
+				"parentName": "GridContainer_uxln7d4",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "SQLText",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 6
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_645ba5g",
+					"control": "$PageParameters_UsrTextParameter1_645ba5g",
+					"placeholder": "",
+					"tooltip": "",
+					"readonly": true,
+					"multiline": true,
+					"labelPosition": "auto",
+					"visible": true
+				},
+				"parentName": "GridContainer_uxln7d4",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrIntegerParameter1_es6vo2y": {
+						"modelConfig": {
+							"path": "PageParameters.UsrAvgTicketPrice"
+						}
+					},
+					"PageParameters_UsrTextParameter1_645ba5g": {
+						"modelConfig": {
+							"path": "PageParameters.UsrSqlText"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
